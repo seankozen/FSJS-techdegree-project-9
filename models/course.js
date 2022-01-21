ab9129cb890fe969f969d04541b20c7fb97a1036
Sean Kozen <sgkozen@gmail.com>
2022-01-21 14:05:53 +0900

    All working

--- models/course.js
+++ models/course.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const { Model, DataTypes } = require('sequelize');
+//const sequelize = require('sequelize');
+//const bcrypt = require('bcrypt');
 
 module.exports = (sequelize) => {
     class Course extends Model{};
--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,10 @@
 let express = require('express');
 let router = express.Router();
 const { User, Course } = require('../models');
-const Sequelize = require('sequelize');
-const user = require('../models/user');
+//const Sequelize = require('sequelize');
+//const user = require('../models/user');
 const { authenticateUser } = require('../middleware/auth-user');
+const { json } = require('express/lib/response');
 
 // Async handler for routes
 function asyncHandler(cb){
@@ -52,36 +53,57 @@ router.get('/:id', asyncHandler( async(req, res) => {
 
 
 // Create new course with user authentication
-router.post('/:id', authenticateUser, asyncHandler( async(req, res) => {
+router.post('/', authenticateUser, asyncHandler( async(req, res) => {
 	try {
-		const user = req.currentUser;
-		const course = await Course.findByPk(req.params.id);
-
+		const course = await Course.create(req.body);
+		res.status(201).location(`/api/courses/${course.id}`).end();
 	} catch (error) {
-
-
-
+		console.log('Error:', error.name);
+		if(error.name === 'SequelizeValidationError'){
+			const errors = error.errors.map(err = err.message);
+			res.status(400).json({errors});
+		} else {
+			throw error;
+		}
 	}
-
 }));
 
 // Updates a course and includes user authentication  
 router.put('/:id', authenticateUser, asyncHandler( async(req, res) => {
 	try {
-		
-
+		const user = req.currentUser;
+		const course = await Course.findByPk(req.params.id);
+		if(course && course.userId === user.id) {
+			await course.update(req.body);
+			res.status(204).end();
+		} else {
+			const error = new Error();
+			error.message = "You are not authorized to make changes to this course."
+			res.status(403).json({message: error.message});
+		}		
 	} catch (error) {
-
-
-
+		console.log('Error:', error.name)
+		if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+			const errors = error.errors.map( err => err.message);
+			res.status(400).json({errors});	
+		} else {
+			throw error;
+		}
 	}
-
 }));
 
 // Deletes a course and includes user authentication  
 router.delete('/:id', authenticateUser, asyncHandler( async(req, res) => {
-	
-
+	const user = req.currentUser;
+	const course = await Course.findByPk(req.params.id);
+	if(course && course.userId === user.id){
+		await course.destroy();
+		res.status(204).end();
+	} else {
+		const error = new Error();
+		error.message = "You are not authorized to delete this course!";
+		res.status(403).json({message: error.message});
+	}
 }));
 
 
